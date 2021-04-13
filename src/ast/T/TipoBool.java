b64cb763d1f625ec035f12bbe83acccc9e0ec442
Pablo <psanzc@hotmail.com>
2021-04-13 18:25:50 +0200

    Tipos

---
+++ src/ast/T/TipoBool.java
@@ -0,0 +1,17 @@
+package ast.T;
+
+public class TipoBool extends Tipo{
+
+    @Override
+    public EnumeradoTipo tipoEnumerado() {
+        // TODO Auto-generated method stub
+        return EnumeradoTipo.BOOLEAN;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return "Bool";
+    }
+    
+}
