1d01f48811aa30d85f32bff3018c59e7c8c93eae
Daniel Valverde <davalve@amazon.es>
2021-04-23 00:03:26 +0200

    Main cambiado y ready para entrega II

--- src/constructorast/Main.java
+++ src/constructorast/Main.java
@@ -11,20 +11,19 @@ import ast.I.I;
 import errors.GestionErrores;
 
 public class Main {
-    //Funcion auxiliar encargada de separar obtener los hijos de un nodo del arbol
-    public static List<String> splitFromParent(String parent){
-        List<String> children = new ArrayList<String>();
+    public static List<String> splitThree(String three){
+        List<String> children = new ArrayList<>();
         int begin_inst_index, end_inst_index, counter = 0, i = 0;
 
         begin_inst_index = i;
-        while(i < parent.length()) {
-            char aux = parent.charAt(i);
-            if(aux == '{') counter++;
-            else if(aux == '}') counter--;
+        while(i < three.length()) {
+            if(three.charAt(i) == '{') counter++;
+            else if(three.charAt(i) == '}') counter--;
 
-            if(counter == 0 && aux == '}') { //new instruction
+            //Nueva instruccion
+            if(counter == 0 && three.charAt(i) == '}') {
                 end_inst_index = i;
-                children.add(parent.substring(begin_inst_index+1, end_inst_index));
+                children.add(three.substring(begin_inst_index+1, end_inst_index));
                 begin_inst_index = i+1;
             }
             else if(counter == 0) begin_inst_index++;
@@ -33,29 +32,34 @@ public class Main {
         return children;
     }
 
-    //Funcion encargada de dibujar el AST
     public static String printTree(String raiz, List<String> hijos, String nivel_actual, boolean last_child) {
         int tam = hijos.size();
-        List<String> aux;
+        List<String> hijosDelHijo;
         String total = "", nivel_siguiente = "";
 
         nivel_siguiente = nivel_actual + "\t|";
 
-        //Dibujamos la raiz
+        //Raiz
         if(!last_child) total += nivel_actual + raiz + "\n";
         else total += nivel_actual + "|" + raiz + "\n";
-        for(int i = 0; i < tam; i++) { //Para cada hijo
-            aux = splitFromParent(hijos.get(i)); //Obtenemos los hijos del hijo
-            if(aux.isEmpty()) { //Si no tiene -> es una hoja -> la imprimimos
+
+        //Iteramos hijos
+        for(int i = 0; i < tam; i++) {
+            hijosDelHijo = splitThree(hijos.get(i));
+            //Hoja
+            if(hijosDelHijo.isEmpty()) {
                 total += nivel_siguiente +  hijos.get(i) + "\n";
             }
-            else { //Si tiene -> es un subarbol -> recursion
+            //Subarbol
+            else {
+                //Último arbol -> Quitamos el nivel (|)  actual para los siguientes prints
                 if(i == tam-1) {
                     nivel_siguiente = nivel_siguiente.substring(0, nivel_siguiente.length()-1);
                     last_child = true;
                 }
+                //No último hijo
                 else last_child = false;
-                total += printTree(aux.get(0), aux.subList(1, aux.size()), nivel_siguiente, last_child);
+                total += printTree(hijosDelHijo.get(0), hijosDelHijo.subList(1, hijosDelHijo.size()), nivel_siguiente, last_child);
             }
         }
         return total;
@@ -71,10 +75,11 @@ public class Main {
 
 	 if(GestionErrores.numeroErroresSintacticos == 0){
 	     System.out.println("Análisis lexico/sintáctico realizado sin errores.");
-	     System.out.println(programa);
          System.out.println("Arbol sintático:");
+         System.out.println(programa);
+
          String tree = programa.toString().substring(1, programa.toString().length()-1);
-         System.out.println(printTree("PROGRAMA", splitFromParent(tree), "", true));
+         System.out.println(printTree("PROGRAMA", splitThree(tree), "", true));
 
      }
  }
