3fba8b06347739fea02d001d1dde96172d65b184
Daniel Valverde <davalve@amazon.es>
2021-05-03 11:22:40 +0200

    Estructura asem

--- src/asem/AnalizadorSemantico.java
+++ src/asem/AnalizadorSemantico.java
@@ -5,16 +5,29 @@ import ast.I.*;
 import ast.T.*;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class AnalizadorSemantico {
     private List<I> programa;
     private TablaSimbolos tabla = new TablaSimbolos();
+    private ComprobadorTipos comprobadorTipos = new ComprobadorTipos();
 
     public AnalizadorSemantico(List<I> programa){
         this.programa = programa;
     }
 
     public boolean analizar(){
-        return true;
+        tabla.abreBloque();
+        for(I  instruccion : programa) vincula(instruccion);
+        tabla.cierraBloque();
+
+        AtomicBoolean tiposCorrectos = new AtomicBoolean(true);
+        programa.forEach(x -> {tiposCorrectos.set(comprobadorTipos.comprueba(x) && tiposCorrectos.get());});
+        if(tiposCorrectos.get()) System.out.println("Comprobacion de Tipos sin errores");
+        return tiposCorrectos.get();
+    }
+
+    private void vincula(I instruccion) {
+
     }
 }
---
+++ src/asem/ComprobadorTipos.java
@@ -0,0 +1,9 @@
+package asem;
+
+import ast.I.I;
+
+public class ComprobadorTipos {
+    public boolean comprueba(I instruccion){
+        return true;
+    }
+}
