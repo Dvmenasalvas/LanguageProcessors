8aeb95387029ed0fd1d69687549f92e190162ecc
Daniel Valverde <davalve@amazon.es>
2021-05-03 19:41:16 +0200

    Avances semántico

--- src/asem/ComprobadorTipos.java
+++ src/asem/ComprobadorTipos.java
@@ -4,6 +4,23 @@ import ast.I.I;
 
 public class ComprobadorTipos {
     public boolean comprueba(I instruccion){
+        switch (instruccion.tipoInstruccion()){
+            case ASIG:
+               break;
+            case LLAMDADAPROC:
+                break;
+            case DECL:
+                break;
+            case DECLFUN:
+                break;
+            case STRUCT:
+                break;
+            case SWITCH:
+                break;
+            case WHILE:
+                break;
+
+        }
         return true;
     }
 }
